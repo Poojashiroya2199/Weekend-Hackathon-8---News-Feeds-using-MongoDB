d2330c3ae9dd91626e83b7d61eddd6e076161965
Pooja Shiroya <poojashiroya99@gmail.com>
2020-12-11 21:43:15 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 8080
+// const express = require('express')
+// const app = express()
+// const port = 8080
+// const {newsArticleModel}=require('./connector');
+// const onePageArticleCount = 10
+
+
+// // Parse JSON bodies (as sent by API clients)
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// app.get('/newFeeds',(req,res)=>{
+//     const limit=!is.NaN(req.query.limit)|| req.query.limit?10:parseInt(req.query.limit);
+//     const offset=!is.NaN(req.query.offset)|| req.query.offset?10:parseInt(req.query.offset);
+//     newsArticleModel.find().limit(limit).skip(offset).then((ele)=>{
+//         res.send(ele);
+//     })
+// })
+
+// app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+// module.exports = app;
+const express = require('express');
+const app = express();
+const port = 8080;
 const {newsArticleModel}=require('./connector');
+
 const onePageArticleCount = 10
 
 
@@ -10,12 +33,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/newFeeds',(req,res)=>{
-    const limit=!is.NaN(req.query.limit)|| req.query.limit?10:parseInt(req.query.limit);
-    const offset=!is.NaN(req.query.offset)|| req.query.offset?10:parseInt(req.query.offset);
+    let limit=isNaN(req.query.limit) || !req.query.limit ?10:parseInt(req.query.limit);
+    let offset=isNaN(req.query.offset) || !req.query.offset ?0:parseInt(req.query.offset);
+
     newsArticleModel.find().limit(limit).skip(offset).then((ele)=>{
         res.send(ele);
-    })
-})
+    });
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
